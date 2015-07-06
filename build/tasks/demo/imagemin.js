2197c329acb6732fd8fec9101c726138960c078d
Aaike Van Roekeghem <aaikevr@gmail.com>
2015-07-07 02:23:22 +0700

    Update 2015-07-06T19:23:15.616Z

--- build/tasks/demo/imagemin.js
+++
@@ -1,8 +0,0 @@
-var gulp = require('gulp');
-var fse = require('fs-extra');
-
-var dirs = gulp.pkg.demo.directories;
-
-gulp.task('demo-build-images', function (done) {
-  fse.copy(dirs.lib+"/images",dirs.build+"/images",done);
-});
--- src/index.js
+++
@@ -1,7 +0,0 @@
-import {AceEditor} from "./ace";
-export {AceEditor} from "./ace";
-
-export function configure(aurelia, cb){
-  aurelia.globalizeResources("./ace");
-  if(cb !== undefined && typeof(cb) === 'function') cb(AceEditor);
-}
